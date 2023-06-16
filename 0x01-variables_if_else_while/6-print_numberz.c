ea26b0e76002ec74faef638d799de4bad2d1960a
Ismalure12 <bysalman5@gmail.com>
2023-06-16 16:24:13 -0700

    print digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 48;
+	while (c <= 57)
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
