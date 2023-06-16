935b8a98794b4deaca5dec16f11a076242a036f2
Ismalure12 <bysalman5@gmail.com>
2023-06-16 15:31:10 -0700

    while loop with alphabetical

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
