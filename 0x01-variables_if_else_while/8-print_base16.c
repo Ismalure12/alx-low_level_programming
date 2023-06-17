6d20f9be5d66e418776996c14c4831a4daf0798f
Ismalure12 <bysalman5@gmail.com>
2023-06-16 18:25:43 -0700

    print hexa decimal degits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int a;
+
+	c = 'a';
+	a = 48;
+	while (a < 57)
+	{
+		putchar(a);
+		a = a + 1;
+	}
+	while (c < 'f')
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
